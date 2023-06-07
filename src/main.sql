d8ca0e204b482cbe2b977dd3e1899547bc6dc3a8
Denis <warrynbo@student.21-school.ru>
2023-06-07 18:39:10 +0300

    finish 1,2 parts

--- src/main.sql
+++ src/main.sql
@@ -1,60 +1,70 @@
-CREATE SCHEMA main;
-
-CREATE TABLE main.station (
-	id           SERIAL PRIMARY KEY,
-	name_station VARCHAR(255) NOT NULL,
-	city         VARCHAR(255) NOT NULL,
-	region       VARCHAR(255) NOT NULL
+CREATE TABLE train (
+    id             SERIAL PRIMARY KEY,
+    name_train     VARCHAR NOT NULL,
+    type_train     VARCHAR NOT NULL,
+    capacity_train INT
 );
 
-
-CREATE TABLE main.train (
-	id             SERIAL PRIMARY KEY,
-	name_train     VARCHAR(255) NOT NULL,
-	type_train     VARCHAR(255) NOT NULL,
-	capacity_train INT
+CREATE TABLE station (
+    id           SERIAL PRIMARY KEY,
+    name_station VARCHAR NOT NULL,
+    city         VARCHAR NOT NULL,
+    region       VARCHAR NOT NULL
 );
 
-CREATE TABLE main.route (
-	id                SERIAL PRIMARY KEY,
-	id_start_station  INT NOT NULL,
-	id_finish_station INT NOT NULL,
-	lenght            DECIMAL,
-	constraint fk_route_start_id foreign key (id_start_station) references station (id),
-	constraint fk_route_finish_id foreign key (id_finish_station) references station (id)
+CREATE TABLE route (
+    id                 SERIAL PRIMARY KEY,
+    id_start_station   INT REFERENCES station(id),
+    id_finish_station  INT REFERENCES station(id),
+    length             DECIMAL
 );
 
-CREATE TABLE main.schedule (
-    id SERIAL PRIMARY KEY,
-    id_train INT NOT NULL,
-    id_route INT NOT NULL
+CREATE TABLE schedule (
+    id            SERIAL PRIMARY KEY,
+    id_train      INT REFERENCES train(id),
+    id_route      INT REFERENCES route(id),
+    date_start    DATE NOT NULL,
+    time_start    TIME NOT NULL,
+    date_finish   DATE NOT NULL,
+    time_finish   TIME NOT NULL
 );
 
-CREATE TYPE main.check_ticket_status AS ENUM ('sold', 'free');
+CREATE TYPE check_ticket_status AS ENUM ('sold', 'free');
 
-INSERT INTO main.station (id, name_station, city, region)
-VALUES (1, 'Bot_Sad', 'Moscow', 'CFO'),
-       (2, 'Kazan-2', 'Kazan', 'PFO'),
-       (3, 'Snt.P', 'Saint Peterburg', 'SZFO');
-	
-INSERT INTO main.train (id, name_train, type_train, capacity_train)
+CREATE TABLE ticket (
+    id           BIGSERIAL PRIMARY KEY,
+    id_schedule  INT REFERENCES schedule(id),
+    price        DECIMAL NOT NULL,
+    status       check_ticket_status
+);
+
+INSERT INTO train (id, name_train, type_train, capacity_train)
 VALUES (1, 'M01', 'standart train', 650),
        (2, 'M02', 'express train', 550),
 	   (3, 'K01', 'standart train', 650),
-       (4, 'K02', 'express train', 550),
-	   (5, 'SP01', 'standart train', 650),
-       (6, 'SP02', 'express train', 550);
-	      
-INSERT INTO main.route (id, id_start_station, id_finish_station, lenght)
+       (4, 'K02', 'express train', 550);
+
+INSERT INTO station (id, name_station, city, region)
+VALUES (1, '3-Vokzala', 'Moscow', 'CFO'),
+       (2, 'Kazan-2', 'Kazan', 'PFO');
+    
+INSERT INTO route (id, id_start_station, id_finish_station, lenght)
 VALUES (1, 1, 2, 800.5),
-       (2, 1, 3, 750.6),
-	   (3, 2, 1, 800.5),
-	   (4, 2, 3, 1500.2),
-	   (5, 3, 2, 1500.2),
-	   (6, 3, 1, 750.6);
+	   (2, 2, 1, 800.5),
+
+INSERT INTO schedule (id, id_train, id_route, date_start, time_start, date_finish, time_finish)
+VALUES (1, 1, 1, '2023-06-07', '13:00:00', '2023-06-08', '05:00:00'),
+       (2, 2, 1, '2023-06-07', '15:00:00', '2023-06-07', '23:00:00'),
+       (3, 3, 2, '2023-06-08', '07:00:00', '2023-06-08', '21:00:00'),
+       (4, 4, 2, '2023-06-08', '10:00:00', '2023-06-08', '18:00:00');
+
+INSERT INTO ticket (id, id_schedule, price, status)
+VALUES (1, 1, 1500, 'free'),
+       (2, 1, 1500, 'free'),
+       (3, 1, 1500, 'sold');
 
 CREATE INDEX IF NOT EXISTS idx_schedule_id_train ON schedule USING btree (id_train);
-CREATE INDEX IF NOT EXISTS idx_person_order_person_id ON person_order USING btree (person_id);
-CREATE INDEX IF NOT EXISTS idx_person_order_menu_id ON person_order USING btree (menu_id);
-CREATE INDEX IF NOT EXISTS idx_person_visits_person_id ON person_visits USING btree (person_id);
-CREATE INDEX IF NOT EXISTS idx_person_visits_pizzeria_id ON person_visits USING btree (pizzeria_id);
+CREATE INDEX IF NOT EXISTS idx_schedule_id_route ON schedule USING btree (id_route);
+CREATE INDEX IF NOT EXISTS idx_route_id_start ON route USING btree (id_start_station);
+CREATE INDEX IF NOT EXISTS idx_route_id_finish ON route USING btree (id_finish_station);
+CREATE INDEX IF NOT EXISTS idx_ticket_id_schedule ON ticket USING btree (id_schedule);
--- src/security.sql
+++ src/security.sql
@@ -1,20 +1,20 @@
 CREATE SCHEMA security;
 
-CREATE TABLE security.users (
-  user_id SERIAL PRIMARY KEY,
-  username VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  role_id INT REFERENCES security.roles(role_id)
-);
-
 CREATE TABLE security.roles (
   role_id SERIAL PRIMARY KEY,
-  role_name VARCHAR(255) UNIQUE NOT NULL
+  role_name VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE security.users (
+  user_id SERIAL PRIMARY KEY,
+  username VARCHAR UNIQUE NOT NULL,
+  password VARCHAR NOT NULL,
+  role_id INT REFERENCES security.roles(role_id)
 );
 
 CREATE TABLE security.permissions (
   permission_id SERIAL PRIMARY KEY,
-  permission_name VARCHAR(255) UNIQUE NOT NULL
+  permission_name VARCHAR UNIQUE NOT NULL
 );
 
 CREATE TABLE security.role_permissions (
@@ -23,15 +23,12 @@ CREATE TABLE security.role_permissions (
   PRIMARY KEY (role_id, permission_id)
 );
 
--- Добавление ролей
 INSERT INTO security.roles (role_name) VALUES ('cashier');
 INSERT INTO security.roles (role_name) VALUES ('admin');
 
--- Добавление прав доступа
-INSERT INTO security.permissions (permission_name) VALUES ('check trains');
+INSERT INTO security.permissions (permission_name) VALUES ('check tickets');
 INSERT INTO security.permissions (permission_name) VALUES ('edit schedule');
 
--- Назначение прав доступа для ролей
-INSERT INTO security.role_permissions (role_id, permission_id) VALUES (1, 1); -- роль "кассир" имеет право "просмотр поездов"
-INSERT INTO security.role_permissions (role_id, permission_id) VALUES (2, 1); -- роль "администратор" имеет право "просмотр поездов"
-INSERT INTO security.role_permissions (role_id, permission_id) VALUES (2, 2); -- роль "администратор" имеет право "редактирование расписания"
+INSERT INTO security.role_permissions (role_id, permission_id) VALUES (1, 1);
+INSERT INTO security.role_permissions (role_id, permission_id) VALUES (2, 1);
+INSERT INTO security.role_permissions (role_id, permission_id) VALUES (2, 2);
